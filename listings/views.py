fb91a528ea0ad8201d4e7736017324f772aa746c
MH <hsw_it@protonmail.com>
2022-08-10 09:48:02 +0800

    upload

--- listings/views.py
+++ listings/views.py
@@ -27,4 +27,39 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    query_list = Listing.objects.order_by('-list_date')
+    # keywords search, step 1 make sure no empty strings
+    # step 2 then look up the keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query_list = query_list.filter(desciption__icontains=keywords)
+    # second search field city, need to be exact but case insensitive
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            query_list = query_list.filter(city__iexact=city)
+    # add state search
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            query_list = query_list.filter(state__iexact=state)
+    # search bedrooms with less than option
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            query_list = query_list.filter(bedrooms__lte=bedrooms)
+    # search price less than
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            query_list = query_list.filter(price__lte=price)
+    # keep request object values
+    context = {
+        'state_choices': state_choices,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'listings': query_list,
+        'values': request.GET
+    }
+    return render(request, 'listings/search.html', context)
